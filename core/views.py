4ee800d84fd14e977c0253368cea7b59c02b2184
SanjaLV <GhostSanja@gmail.com>
2019-03-31 15:38:19 +0300

    Rewrite html as template.
    It will be easier to update template in future.

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 from .models import Game
@@ -8,7 +9,10 @@ from .models import Game
 def index(request):
 
     latest_game_list = Game.objects.order_by("-date")[:5]
-    output = "<h1>Latest games:</h1> <br>\n"
-    for game in latest_game_list:
-        output += str(game) + "<br>\n"
-    return HttpResponse(output)
+    template = loader.get_template("core/index.html")
+
+    context = {
+        'games': latest_game_list,
+    }
+
+    return HttpResponse(template.render(context, request))
