deeccbec9013a41cb187a29b0621f51f912a2995
SanjaLV <GhostSanja@gmail.com>
2019-03-31 13:32:18 +0300

    Player and Game models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Player(models.Model):
+    name   = models.CharField(max_length=30)
+    elo    = models.IntegerField()
+    userID = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+class Game(models.Model):
+    player1 = models.ForeignKey(Player, on_delete=models.PROTECT, related_name="player_one")
+    player2  = models.ForeignKey(Player, on_delete=models.PROTECT, related_name="player_two")
+    score1   = models.IntegerField(default=0)
+    score2   = models.IntegerField(default=0)
+    elo1     = models.IntegerField()
+    elo2     = models.IntegerField()
+    change   = models.IntegerField()
+    date     = models.DateTimeField()
+    verified = models.BooleanField(default=False)
+
