321402ba34c9043fdeab7c92ab856fb594177caa
SanjaLV <GhostSanja@gmail.com>
2019-04-24 22:19:53 +0300

    Add set score for game.

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('game/<int:game_id>', views.game_data, name="game"),
     path('player/<int:player_id>', views.player_data, name="player"),
-    path('register', views.register, name="registration")
+    path('register', views.register, name="registration"),
+    path('game/<int:game_id>/set_score', views.set_game_score, name="set_game_score")
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ObjectDoesNotExist
@@ -6,6 +7,7 @@ from django.http import HttpResponse, HttpResponseForbidden
 from django.template import loader
 from django.db.models import Q
 
+import core.errors
 from .models import Game, Player
 
 
@@ -21,7 +23,7 @@ def index(request):
 
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
@@ -39,7 +41,7 @@ def game_data(request, game_id):
     try:
         this_game = Game.objects.get(pk=game_id)
     except ObjectDoesNotExist:
-        return HttpResponseForbidden(request)
+        return HttpResponseForbidden(core.errors.THERE_IS_NO_GAME)
 
     template = loader.get_template("core/game.html")
 
@@ -53,12 +55,12 @@ def player_data(request, player_id):
     try:
         this_player = Player.objects.get(pk=player_id)
     except ObjectDoesNotExist:
-        return HttpResponseForbidden(request)
+        return HttpResponseForbidden(core.errors.THERE_IS_NO_PLAYER)
 
     query = Q(player1=this_player.pk)
     query.add(Q(player2=this_player.pk), Q.OR)
 
-    last_games = Game.objects.filter(query)[:5]
+    last_games = Game.objects.filter(query).order_by("-date")[:5]
 
     template = loader.get_template("core/player.html")
     context = {
@@ -68,3 +70,46 @@ def player_data(request, player_id):
         'user_id': this_player.pk
     }
     return HttpResponse(template.render(context, request))
+
+
+def validate_score(post_data):
+    if "score1" not in post_data:
+        return False, "There is no player one score!"
+    if "score2" not in post_data:
+        return False, "There is no player two score!"
+    try:
+        s = int(post_data["score1"])
+        s = int(post_data["score2"])
+    except TypeError:
+        return False, "Data is not int"
+    return True, ""
+
+
+def set_game_score(request, game_id):
+    if request.method == "POST":
+        post_data = request.POST
+
+        if request.user.is_authenticated:
+            current_user = request.user
+        else:
+            return HttpResponseForbidden(request)
+        try:
+            this_game = Game.objects.get(pk=game_id)
+        except ObjectDoesNotExist:
+            return HttpResponseForbidden(core.errors.THERE_IS_NO_GAME)
+        if this_game.player1.pk != current_user.pk:
+            return HttpResponseForbidden(core.errors.YOU_ARE_NOT_ALLOWED)
+
+        print(post_data)
+
+        flag, res = validate_score(post_data)
+
+        if flag:
+            this_game.score1 = int(post_data["score1"])
+            this_game.score2 = int(post_data["score2"])
+            this_game.save()
+            messages.success(request, "Scores updated.")
+        else:
+            messages.warning(request, "ERROR!" + res)
+
+    return game_data(request, game_id)
