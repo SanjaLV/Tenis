cd37d1ec741efebee78d4a35e79d4a8b93661354
SanjaLV <GhostSanja@gmail.com>
2019-05-03 00:51:03 +0300

    Changed core.errors.YOU_ARE_NOT_ALLOWED to -> Access denied!

--- core/errors.py
+++ core/errors.py
@@ -1,6 +1,6 @@
 
 
 THERE_IS_NO_GAME = "<h2>ERROR! There is no game with this id!</h2>"
-YOU_ARE_NOT_ALLOWED = "<h2> ERROR! You are not allowed to view this page!</h2>"
+YOU_ARE_NOT_ALLOWED = "<h2> ERROR! Access denied</h2> <img height=""90%"" src=""https://www.publicdomainpictures.net/pictures/240000/velka/man-hold-stop-sign.jpg"">"
 THERE_IS_NO_PLAYER = "<h2>ERROR! There is no player with this id!</h2>"
 YOU_MUST_LOGIN = "<h2>ERROR! You must be authenticated!</h2>"
