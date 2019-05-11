294304602d91b1f8e66651f95f60c2b11b7d3887
SanjaLV <GhostSanja@gmail.com>
2019-05-11 22:21:30 +0300

    Created unit-tests for players module.

--- core/tests.py
+++ core/tests.py
@@ -1,8 +1,10 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
 
 # Create your tests here.
+import core.errors
 from core.management.commands.import_games import normalize_name
-from .models import Game, Player
+from .models import Game, Player, Statistic
 
 
 class TestGame(TestCase):
@@ -38,3 +40,81 @@ class TestNormalizeName(TestCase):
         self.assertEqual(normalize_name("   A A a A"), "A A A A")
         self.assertEqual(normalize_name("sanja"), "Sanja")
         self.assertEqual(normalize_name(" Edgars K"), "Edgars K")
+
+
+class TestPlayer(TestCase):
+    def setUp(self):
+        self.John = User.objects.create_user(username="John", password="Dhuhg437fus")
+        self.Max = User.objects.create_user(username="Max", password="adjd8j82hDao")
+
+    def test_create_player(self):
+        self.longMessage = True
+        c = Client()
+
+        response = c.post('/core/new_player')
+        self.assertEqual(response.status_code, 403)
+
+        c.login(username="John", password="Dhuhg437fus")
+
+        response = c.get('/core/new_player')
+        self.assertEqual(response.status_code, 200)
+
+        response = c.post('/core/new_player', {'name': 'player_one'})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/core/player/1')
+
+        response = c.post('/core/new_player', {'name': 'player_two'})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/core/player/2')
+
+        response = c.post('/core/new_player', {'name': ''})
+        self.assertEqual(response.status_code, 200)
+
+        response = c.get('/core/player/3')
+        self.assertEqual(response.status_code, 403)
+
+        response = c.post('/core/new_player', {'name': 'player_one'})
+        self.assertEqual(response.status_code, 200)
+
+        response = c.get('/core/player/3')
+        self.assertEqual(response.status_code, 403)
+
+    def test_my_players(self):
+        c = Client()
+
+        p1 = Player.objects.create(name="John", user=self.John)
+        s1 = Statistic.objects.create(player=p1)
+        p2 = Player.objects.create(name="Max", user=self.Max)
+        s2 = Statistic.objects.create(player=p2)
+        p3 = Player.objects.create(name="Max2", user=self.Max)
+        s3 = Statistic.objects.create(player=p3)
+
+        response = c.get('/core/players')
+        self.assertEqual(response.status_code, 403)
+
+        c.login(username="John", password="Dhuhg437fus")
+
+        response = c.get('/core/players')
+        self.assertEqual(response.status_code, 200)
+
+        should_be_p = [p1]
+        should_be_s = [s1]
+        iteration = 0
+        for p, s in response.context["players_stats"]:
+            self.assertEqual(p, should_be_p[iteration])
+            self.assertEqual(s, should_be_s[iteration])
+            iteration += 1
+
+        c.login(username="Max", password="adjd8j82hDao")
+
+        response = c.get('/core/players')
+        self.assertEqual(response.status_code, 200)
+
+        should_be_p = [p2, p3]
+        should_be_s = [s2, s3]
+
+        for p, s in response.context["players_stats"]:
+            self.assertEqual(p, should_be_p[iteration])
+            self.assertEqual(s, should_be_s[iteration])
+            iteration += 1
+
